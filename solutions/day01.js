a4ccd8a86279e1b53a4ae34b44fa00ef2da041d0
wksnders <42388072+wksnders@users.noreply.github.com>
2024-12-01 22:53:45 -0700

    inital test solved

--- solutions/day01.js
+++ solutions/day01.js
@@ -7,28 +7,38 @@ const testinput =
 3   3`;
 
 const testOutput = 11;
+const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
 
+const trace = msg => x => {
+    console.log(msg, x);
+    return x;
+  }
 
-export function solveDay1(input) {
-    let a = [];
-    let b = [];
-    input.split('\n')
-    .map((l) => l.split("   ").map(toNumber));//[num][num]
+function solveDay1(input) {
+    
+    const lists = input.split('\n')
+        .map((l) => l.split("   ")
+            .map((num) => 
+            pipe(
+                /*trace('num'),*/
+                x => +x,
+            )(num))
+        );
+    //TODO how can I make this work for arbitrary num lists?
+    let a = lists.map((line)=>line[0]).sort();
+    let b = lists.map((line)=>line[1]).sort();
 
-    return 0; 
-}
+    return a.reduce((total,numA,index) => total + distance(numA,b[index]),0);
+}   
+
 
 function distance(a, b) {
+    console.log(a,b,'dist : ', Math.abs(a - b))
     return Math.abs(a - b);
-}
-
-function buildList(a){
-    return function(b){
-        return [...b,a]
-    }
-
 }
 
 function toNumber(x){
     return +x;
 }
+
+console.log(solveDay1(testinput));
